709aa2fff74b86d9ede2a43f26b57fc5f38e5fb5
Ricardo Cuan <ricardo.cuan@aiesec.net>
2020-11-18 23:30:44 -0500

    📦 NEW: Landing

---
+++ src/Landing/Landing.tsx
@@ -0,0 +1,27 @@
+
+/** Landing Pages
+ * 
+ * @class Landing
+ * @author Ricardo Cuan {@link ricardo.cuan@utp.ac.pa}
+ * @category Páginas
+ */
+import React, { Component } from 'react'
+import './style.scss'
+
+interface Props {}
+
+interface State {}
+
+class Landing extends Component<Props, State> {
+  state = {}
+
+  render() {
+    return (
+      <div>
+        <h1>Landing 🤠</h1>
+      </div>
+    )
+  }
+}
+
+export default Landing
